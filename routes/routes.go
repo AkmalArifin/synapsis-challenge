c6e478fa47e1703898d4175e8ecbe5a8197a95a2
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2024-12-05 11:00:41 +0700

    feat: add routes

--- routes/routes.go
+++ routes/routes.go
@@ -11,4 +11,17 @@ func RegisterRoutes(r *gin.Engine) {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "pong"})
 	})
+
+	r.POST("/login", login)
+	r.POST("/register", register)
+
+	r.GET("/products/:id", getProductsByCategory)
+
+	r.POST("/payments", checkoutPayment)
+
+	r.POST("/orders", checkoutOrder)
+
+	r.GET("/cart-items/:id", getCartItemsByUser)
+	r.POST("/cart-items/", createCartItem)
+	r.DELETE("/cart-items/:id", deleteCartItem)
 }
